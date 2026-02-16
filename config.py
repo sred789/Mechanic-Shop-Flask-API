12bf2d77c99f2aefb74d33c589442ecc9ecaa319
seanr789 <seanreddy2000@gmail.com>
2026-02-16 16:11:11 -0600

    updated production config

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///service_shop.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-me")
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    CACHE_TYPE = "SimpleCache"
+    HOST = os.environ.get('HOST', 'localhost')
+    PORT = os.environ.get('PORT', 443)
